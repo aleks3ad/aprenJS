8b151256b16575ae3d717eb25a6cb14087f46659
aleks3ad <66698093+aleks3ad@users.noreply.github.com>
2022-12-04 20:50:45 +0000

    actualizado

--- 02tipoVar.js
+++ 02tipoVar.js
@@ -1,5 +1,9 @@
 // 'var' en desuso
 // 'let' ahora para asignar var
 //'const' es una constante
-let nom = "ayala";
+let a = 3, b = 3, c="3";
+nom = a==c; //revisa el valor, no importa el tipo
 console.log(nom);
+nom = a===c;//revisa el valor, importa el tipo
+console.log(nom);
+
