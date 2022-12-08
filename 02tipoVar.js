430e968097a89b06cbc26911d49614e2d308a0b9
aleks3ad <66698093+aleks3ad@users.noreply.github.com>
2022-12-08 03:09:11 +0000

    07/12/22

--- 02tipoVar.js
+++ 02tipoVar.js
@@ -7,3 +7,34 @@ console.log(nom);
 nom = a===c;//revisa el valor, importa el tipo
 console.log(nom);
 
+//convert string a integer
+a ="10+"
+a = Number(a)
+if(isNaN(a)){
+    console.log(a + " no es numero oe")
+} else {
+    console.log(a + " es un numero")
+}
+
+//uso de continue
+
+for(let i=0; i<=10; i++){
+    if(i % 2 !==0){ //si es impar continuar index
+        continue;
+    }
+    console.log(i);
+}
+
+//funciones 1
+ a = function hola(){return "abc"};
+
+ //funciones 2
+ function abc(a=1,b=2){
+    console.log(arguments[0]);
+    console.log(arguments[2]);
+ }
+ abc(2,4,5)
+
+  //funciones 3
+  a = (a,b) => a + b;
+  console.log(a(5,7));
