ddf4349b99e6dfbb4c8904f42670b87deb227068
aleks3ad <66698093+aleks3ad@users.noreply.github.com>
2022-12-10 03:37:53 +0000

    09/12/2022

--- 02tipoVar.js
+++ 02tipoVar.js
@@ -10,31 +10,40 @@ console.log(nom);
 //convert string a integer
 a ="10+"
 a = Number(a)
-if(isNaN(a)){
+if(isNaN(a))
     console.log(a + " no es numero oe")
-} else {
+else
     console.log(a + " es un numero")
-}
 
 //uso de continue
-
 for(let i=0; i<=10; i++){
-    if(i % 2 !==0){ //si es impar continuar index
+    if(i % 2 !==0) //si es impar continuar index
         continue;
-    }
     console.log(i);
 }
 
 //funciones 1
- a = function hola(){return "abc"};
+a = function hola(){return "abc"};
 
- //funciones 2
- function abc(a=1,b=2){
-    console.log(arguments[0]);
-    console.log(arguments[2]);
- }
- abc(2,4,5)
+//funciona 2
+function suma(){
+    let sum = 0;
+    for(let i=0; i<arguments.length; i++)
+        sum += arguments[i];
+    return sum;
+}
+console.log(suma(5,8,6));
 
-  //funciones 3
-  a = (a,b) => a + b;
-  console.log(a(5,7));
+//funciones 3
+a = (a,b) => a + b;
+console.log(a(5,7));
+
+//object
+persona = new Object();
+persona.nom ="ale"
+persona.edad = 12
+delete persona.nom;
+for(n in persona){
+    console.log(persona[n]);
+}
+
